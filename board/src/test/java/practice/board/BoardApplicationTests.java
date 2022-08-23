c7ff179e1a7990e204fb406cae802ae38d1adf33
leewoojin <zmfpdl64@naver.com>
2022-08-24 02:18:02 +0900

    #7 - 오류발생 때문에 @Test 제거
    @Test때문에 sql datasource 등 에러가 발생했다.
    해당 문제는 나중에 찾아 봐야할 것 같다.
    TODO: 왜 문제가 발생했는지 찾아보기

--- board/src/test/java/practice/board/BoardApplicationTests.java
+++ board/src/test/java/practice/board/BoardApplicationTests.java
@@ -3,10 +3,10 @@ package practice.board;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class BoardApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
