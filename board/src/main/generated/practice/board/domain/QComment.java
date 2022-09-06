73016da4b243bda53df60c80d4a5bfd80a275060
leewoojin <zmfpdl64@naver.com>
2022-09-06 22:30:29 +0900

    #27 - Querydsl을 사용하기위한 QClass
    Querydsl을 이용하여 검색을 하기 위해 클래스들을
    생성했다.

---
+++ board/src/main/generated/practice/board/domain/QComment.java
@@ -0,0 +1,70 @@
+package practice.board.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QComment is a Querydsl query type for Comment
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QComment extends EntityPathBase<Comment> {
+
+    private static final long serialVersionUID = 260072944L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QComment comment = new QComment("comment");
+
+    public final QAuditingFileds _super = new QAuditingFileds(this);
+
+    public final QArticle articleId;
+
+    public final NumberPath<Long> commentId = createNumber("commentId", Long.class);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    //inherited
+    public final StringPath modifiedBy = _super.modifiedBy;
+
+    public final QUserAccount userId;
+
+    public QComment(String variable) {
+        this(Comment.class, forVariable(variable), INITS);
+    }
+
+    public QComment(Path<? extends Comment> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QComment(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QComment(PathMetadata metadata, PathInits inits) {
+        this(Comment.class, metadata, inits);
+    }
+
+    public QComment(Class<? extends Comment> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.articleId = inits.isInitialized("articleId") ? new QArticle(forProperty("articleId"), inits.get("articleId")) : null;
+        this.userId = inits.isInitialized("userId") ? new QUserAccount(forProperty("userId")) : null;
+    }
+
+}
+
